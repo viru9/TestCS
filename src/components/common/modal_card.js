0b69a33c5ef861e1955de42ac6c744a0ed4846ed
viru9 <steelviraj@gmail.com>
2019-02-27 21:44:01 +0530

    card loading added without styles

--- src/components/common/modal_card.js
+++ src/components/common/modal_card.js
@@ -3,7 +3,20 @@ import React from 'react';
 const CardModel = (props) => {
 
   return (
-<div>card</div>
+
+    <div className="col-6 col-md-4">
+      <div className="card" style={{"width": "18rem"}}>
+      <img className="card-img-top" src={props.photo} alt="Card image cap"/>
+      <div className="card-body">
+      <h5 className="card-title">{props.name}</h5>
+      <h6 className="card-title">{props.dob}</h6>
+      <p className="card-text">{props.description}</p>
+      <h6 className="card-title">{props.phone}</h6>
+      <h6 className="card-title">{props.email}</h6>
+      </div>
+      </div>
+    </div>
+
   );
 
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchHomeValues} from '../actions/home'
+import {fetchHomeValues} from '../actions/home';
+import ModalCards from './common/modal_card';
+import _ from 'lodash';
+
 
 class Home extends Component {
 
-
   componentDidMount(){
     this.props.fetchHomeValues();
+  }
 
+  renderUserCards(){
+    return _.map(this.props.home.home.response, data => {
+      return (
+        <ModalCards
+        key={data.NAME}
+        name={data.NAME}
+        location={data.LOCATION}
+        dob={data.DOB}
+        photo={data.PHOTO}
+        description={data.DESCRIPTION}
+        phone={data.PHONE}
+        email={data.EMAIL}
+        />
+      )
+    });
   }
 
   render() {
     return (
-      <div>
-aaaaa
+      <div className="container">
+      <div className="row">
+        {this.renderUserCards()}
+      </div>
       </div>
     );
   }
