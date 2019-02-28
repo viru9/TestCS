a2284c7439f380cabee02e091156226c1eea80bb
viru9 <steelviraj@gmail.com>
2019-02-28 22:58:46 +0530

    last css changes added

--- src/components/common/modal_card.js
+++ src/components/common/modal_card.js
@@ -5,16 +5,36 @@ const CardModel = (props) => {
   return (
 
     <div className="col-6 col-md-4 user-card">
-      <div className="card" style={{"width": "18rem"}}>
-      <img className="card-img-top" src={props.photo} alt="Card image cap"/>
-      <div className="card-body">
-      <h5 className="card-title">{props.name}</h5>
-      <h6 className="card-title">{props.dob}</h6>
-      <p className="card-text">{props.description}</p>
-      <h6 className="card-title">{props.location}</h6>
-      <h6 className="card-title">{props.phone}</h6>
-      <h6 className="card-title">{props.email}</h6>
+      <div className="card" style={{"width": "22rem"}}>
+
+
+     <div className="card-header">
+      <div className="row">
+        <div className="col-6 col-md-6">
+        <img className="card-img-top" src={props.photo} alt="Card image cap"/>
+        </div>
+        <div className="col-6 col-md-6">
+        <label className="card-name">{props.name}</label>
+        <label className="card-location-dob"><i className="fas fa-map-marker-alt"/>&nbsp;{props.location}</label>
+        <label className="card-location-dob"><i className="fas fa-birthday-cake"/>&nbsp;{props.dob}</label>
+        </div>
       </div>
+    </div>
+
+      <div className="card-body">
+      <p className="card-description">{props.description}</p>
+      </div>
+
+      <div className="card-footer">
+      <div className="row">
+      <label className="card-phone-email"><i className="fas fa-phone"/>&nbsp;{props.phone}</label>
+      </div>
+      <div className="row">
+      <label className="card-phone-email"><i className="fas fa-envelope"/>&nbsp;{props.email}</label>
+      </div>
+      </div>
+
+
       </div>
     </div>
 
